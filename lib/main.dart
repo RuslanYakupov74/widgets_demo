34428072ed36b255a23bed006b4e5ac9c33aa483
RuslanYakupov74 <98980232+RuslanYakupov74@users.noreply.github.com>
2022-02-26 20:05:54 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_demo/case_2_3.dart';
 
+import 'case_2_4.dart';
 import 'case_2_5.dart';
 
 void main() {
-  runApp(const MyStatefulWidget());
+  runApp(const ThemesDemoApp());
 }
