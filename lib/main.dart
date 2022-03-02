bd371b9adaa04b5c1c4af8e8e7844ef1439ecc18
RuslanYakupov74 <98980232+RuslanYakupov74@users.noreply.github.com>
2022-03-02 20:05:40 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_demo/case_2_3.dart';
+import 'package:widgets_demo/case_3_1_shared_preferences.dart';
 
 import 'case_2_4.dart';
 import 'case_2_5.dart';
+import 'case_3_1_read_and_write_files.dart';
 
 void main() {
-  runApp(const ThemesDemoApp());
+  runApp(const FilesDemoScreen());
 }
