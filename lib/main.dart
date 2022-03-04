20a7057bacb964945a80cc63c49d433209c997e2
RuslanYakupov74 <98980232+RuslanYakupov74@users.noreply.github.com>
2022-03-04 18:52:37 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import 'package:widgets_demo/case_3_1_shared_preferences.dart';
 import 'case_2_4.dart';
 import 'case_2_5.dart';
 import 'case_3_1_read_and_write_files.dart';
+import 'case_3_2.dart';
 
 void main() {
-  runApp(const FilesDemoScreen());
+  runApp(const MyApp());
 }
